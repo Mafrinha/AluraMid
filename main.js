246c49e6800c3c4c78c91ba45b3f05dfee176b6b
Giovanna Mafra <106742745+Mafrinha@users.noreply.github.com>
2023-11-10 08:27:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play('');
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio).play('');
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        alert('Elemento não encontrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -16,6 +22,14 @@ for (contador = 0; contador < listaDeTeclas.length; contador++){
         tocaSom(idAudio);
     };
 
+    tecla.onkeydown = function (){
+        if(evento.code === 'Space'|| evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
 
 listaDeTeclas[1].onclick = tocaSomClap;
